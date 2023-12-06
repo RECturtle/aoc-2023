2c44b92f5c619bf9553ececa175068587671491d
RECturtle <spencer.mertes@gmail.com>
2023-12-05 20:31:41 -0500

    finish day 1

---
+++ day1/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var NumLookup = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
+func readFiles(data []byte) []string {
+	return strings.Split(string(data), "\n")
+}
+
+func dayOne(lines []string) {
+	var sum int
+	for _, v := range lines {
+		codes := regexp.MustCompile(`[^\d]+`).ReplaceAllString(v, "")
+
+		strValue := string(codes[0]) + string(codes[len(codes)-1])
+		calibrationValue, err := strconv.Atoi(strValue)
+
+		if err != nil {
+			fmt.Println("Unable to convert str to int:", err)
+		}
+
+		sum += calibrationValue
+	}
+	fmt.Printf("Day 1: %d\n", sum)
+}
+
+func dayTwo(lines []string) {
+	var sum int
+
+	for _, line := range lines {
+		var matches []string
+
+		re := regexp.MustCompile(`\d|one|two|three|four|five|six|seven|eight|nine`)
+
+		for i := range line {
+			match := re.FindString(line[i:])
+
+			if match == "" {
+				continue
+			}
+
+			if m, ok := NumLookup[match]; ok {
+				match = m
+			}
+
+			matches = append(matches, match)
+		}
+
+		strValue := string(matches[0]) + string(matches[len(matches)-1])
+		calibrationValue, err := strconv.Atoi(strValue)
+
+		if err != nil {
+			fmt.Println("Unable to convert str to int:", err)
+		}
+
+		sum += calibrationValue
+	}
+	fmt.Printf("Day 2: %d\n", sum)
+}
+
+func main() {
+	data, err := os.ReadFile("input1.txt")
+
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+	}
+
+	codes := readFiles(data)
+
+	dayOne(codes)
+	dayTwo(codes)
+}
